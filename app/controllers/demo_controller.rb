00c677e7472a0bf38be521b78a29f9c75b1def03
KrisDu <Kristjan@Kristjans-iMac.local>
2014-07-29 15:32:48 -0700

    Initial Commit

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,18 @@
+class DemoController < ApplicationController
+
+	layout false
+
+  def index
+  	#render('hello')
+  end
+  
+  def hello
+  	#render('index')
+  	@array = [1,2,3,4,5]
+  end
+  
+  def other_hello
+  	redirect_to(:controller => 'demo', :action => 'index')
+  end	
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleCms::Application.config.secret_key_base = 'f4cdd2a0efae7febb1ed0d6ef992840b4cc33f30827e93ada5a3ab3a9c1bcbcfd5b5accf744a4920a057bec1b77a25ff43e1f692fe16e76c5bb45b832155a013'
